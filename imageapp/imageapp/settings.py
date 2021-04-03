4b16b3e0afec11406361e4cae4043c5c28d29d03
Daniil Tolstochenko <pochtobox@mail.ru>
2021-04-03 16:42:24 +0300

    added base template

--- imageapp/imageapp/settings.py
+++ imageapp/imageapp/settings.py
@@ -36,7 +36,7 @@ ROOT_URLCONF = 'imageapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
