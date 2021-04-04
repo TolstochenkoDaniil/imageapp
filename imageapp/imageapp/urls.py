0d765b53bd0685122287d4d35ac981fd5bbe21c7
Daniil Tolstochenko <pochtobox@mail.ru>
2021-04-04 18:47:54 +0300

    added edit templates

--- imageapp/imageapp/urls.py
+++ imageapp/imageapp/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from django.views.generic.base import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 
-from imagemanager.views import ImageUploadView
+from imagemanager.views import ImageUploadView, ImageEditView, ImageListView
 
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='/'),
-    path('image_upload/', ImageUploadView.as_view(), name='image_upload')
+    path('', ImageListView.as_view(template_name='home.html'), name='/'),
+    path('image/upload', ImageUploadView.as_view(), name='upload_image'),
+    path('image/<int:pk>/edit', ImageEditView.as_view(), name='edit_image')
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
